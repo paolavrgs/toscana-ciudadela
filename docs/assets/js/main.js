61cf992641b62dae7e809785dce6b67848b5d8c1
Paola Vargas <vrgspaola68@gmail.com>
2020-04-23 14:02:26 -0400

    Add modal to video, add fixed buttons for whatsapp and go to top

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -7,23 +7,16 @@ WebFont.load({
 });
 
 
-$('.js-gotop').on('click', function(event){
-  event.preventDefault();
-
-  $('html, body').animate({
-    scrollTop: $('html').offset().top
-  }, 500, 'easeInOutExpo');
-
-  return false;
+$(window).scroll(function() {
+  if ($(this).scrollTop()) {
+      $('.js-gotop').fadeIn();
+  } else {
+      $('.js-gotop').fadeOut();
+  }
 });
 
-$(window).scroll(function(){
-  var $win = $(window);
-  if ($win.scrollTop() > 200) {
-    $('.js-top').addClass('active');
-  } else {
-    $('.js-top').removeClass('active');
-  }
+$(".js-gotop").click(function() {
+  $("html, body").animate({scrollTop: 0}, 1000);
 });
 
 $(document).ready(function(){
